3953b70c031d123bd5cd4e4dbf1518b262a450eb
bokwoon <bokwoon.c@gmail.com>
2025-01-25 03:16:00 +0800

    checkpoint

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 set -u
 set -o pipefail
 sudo apt update
-sudo apt install -y wget build-essential libc6-dev libvips libvips-dev libvips-all gpac ffmpeg
+sudo apt install -y wget build-essential libc6-dev libvips libvips-dev gpac ffmpeg
 wget https://raw.githubusercontent.com/FFmpeg/FFmpeg/master/tools/qt-faststart.c
 gcc -O3 -o /usr/local/bin/qt-faststart qt-faststart.c
 rm qt-faststart.c
