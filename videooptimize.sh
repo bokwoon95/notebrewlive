cff7e5c7c667ef5452be913ee862ec3fc674af3d
bokwoon <bokwoon.c@gmail.com>
2025-02-11 01:28:46 +0800

    checkpoint

--- videooptimize.sh
+++ videooptimize.sh
@@ -16,7 +16,9 @@ case "$INPUT_PATH" in
 esac
 video_codec="$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$INPUT_PATH")"
 audio_codec="$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$INPUT_PATH")"
-if test "$video_codec" = "h264" && test "$audio_codec" = "aac" ; then
+if test "$video_codec" = "av1" && test "$audio_codec" = "opus" ; then
+  mv "$INPUT_PATH" "$OUTPUT_PATH"
+elif test "$video_codec" = "h264" && test "$audio_codec" = "aac" ; then
   ffmpeg -hide_banner -loglevel error -i "$INPUT_PATH" -codec copy -movflags +faststart "$OUTPUT_PATH"
 else
   ffmpeg -hide_banner -loglevel error -i "$INPUT_PATH" -codec:v libx264 -preset medium -crf 23 -codec:a aac -b:a 128k -filter:v "format=yuv420p" -movflags +faststart "$OUTPUT_PATH"
